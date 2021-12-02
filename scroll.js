4fbab361084053f9d5c86a27c56a0afa238ba0f2
AvetisHarutyunyan <87875341+Avetis-Harutyunyan@users.noreply.github.com>
2021-12-02 20:03:18 +0400

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function() {
-    const topNav = this.document.querySelector(".header__navbar");
+    const topNav = this.document.querySelector(".header__section");
     topNav.classList.toggle("nav_change", window.scrollY > 130);
 
     const toTop = this.document.querySelector(".toTop");
